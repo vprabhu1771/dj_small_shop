be306f99bafdefc319df1bacf0c95ceb5fa6cf82
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-13 09:40:00 +0530

    Product Added to Model

--- backend/models.py
+++ backend/models.py
@@ -67,3 +67,26 @@ class Brand(models.Model):
 
     class Meta:
         db_table='brand'
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+
+    category=models.ForeignKey(Category,null=True,blank=True,on_delete=models.SET_NULL)
+
+    brand=models.ForeignKey(Brand,null=True,blank=True,on_delete=models.SET_NULL)
+
+    price=models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
+
+    qty=models.IntegerField(null=True,blank=True)
+
+    alert_stock=models.IntegerField(null=True,blank=True)
+
+    image_path = models.ImageField(upload_to='product',null=True,blank=True,default='no-image-available.jpg')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'product'
