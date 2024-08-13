1931969acbd96689fbcc4ee60ad95bba2594fc09
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-13 09:30:56 +0530

    category model added

--- backend/models.py
+++ backend/models.py
@@ -46,3 +46,13 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Category(models.Model):
+    id=models.BigAutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='category'
