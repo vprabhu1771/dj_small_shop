34b615e2083f919bf7839a28e2936593d01d0049
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-13 09:36:24 +0530

    Customer User Added to Admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.utils.html import format_html
+from backend.models import CustomUser
+
+from django.contrib.auth.admin import UserAdmin
+from backend.forms import CustomerUserCreationForm,CustomerUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomerUserCreationForm
+    form = CustomerUserChangeForm
+    model = CustomUser
+    list_display = ('email','gender','image_tag','is_staff','is_active',)
+    list_filter = ('email','is_staff','is_active',)
+    fieldsets = (
+        (None,{'fields': ('email','gender','password')}),
+        ('Permissions',{'fields':('is_staff','is_active', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None,{
+            'classes':('wide',),
+            'fields':('email','gender','password1','password2','is_staff','is_active', 'groups', 'user_permissions')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    def image_tag(self,obj):
+        return format_html('<img src = "{}"width="150" height="150"/>'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+
+admin.site.register(CustomUser,CustomUserAdmin)
