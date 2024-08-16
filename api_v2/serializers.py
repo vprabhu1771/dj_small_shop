fe88553966fa220573aad6083d899c382109f166
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-16 15:41:06 +0530

    Cart Serializer added to api v2

--- api_v2/serializers.py
+++ api_v2/serializers.py
@@ -62,6 +62,44 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
+class CartSerializer(serializers.ModelSerializer):
+    customer_name = serializers.SerializerMethodField()
+    product_id = serializers.SerializerMethodField()
+    product_name = serializers.SerializerMethodField()
+    product_image = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+    def get_customer_name(self, obj):
+        return obj.custom_user.email if obj.custom_user else None
+
+    def get_product_id(self, obj):
+        return obj.product.id if obj.product else None
+
+    def get_product_name(self, obj):
+        return obj.product.name if obj.product else None
+
+    def get_product_image(self, obj):
+        if obj.product and obj.product.images.exists():
+            request = self.context.get('request')
+            image_url = obj.product.images.first().image.url
+            return request.build_absolute_uri(image_url) if request else image_url
+        return None
+
+    # 'product_image']
+
+    # def get_product_image(self, obj):
+    #     request = self.context.get('request')
+    #     if request is not None:
+    #         return request.build_absolute_uri(obj.product.image.url)
+    #     return obj.product.image.url
+
+    def get_price(self, obj):
+        return obj.product.price if obj.product else None
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
